4758801a32e420104a804478b45f194f0d2b4505
Camila Dimas <camiladimas18@gmail.com>
2023-01-30 06:09:59 -0500

    :sparkles: Add elements mapping

--- src/main/java/co/com/booking/userinterface/accommodation_search/ViewBookingPage.java
+++ src/main/java/co/com/booking/userinterface/accommodation_search/ViewBookingPage.java
@@ -1,4 +1,4 @@
-package co.com.booking.userinterface;
+package co.com.booking.userinterface.accommodation_search;
 
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 public class ViewBookingPage {
     public static final Target RESULT_BOOKING = Target.the("result booking with name")
             .located(By.xpath("//android.widget.FrameLayout[@resource-id='com.booking:id/results_list_facet']/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[2]"));
-
 
     private ViewBookingPage() {
     }
--- src/main/java/co/com/booking/userinterface/stays/AccommodationOccupancyPage.java
+++ src/main/java/co/com/booking/userinterface/stays/AccommodationOccupancyPage.java
@@ -6,7 +6,7 @@ import net.serenitybdd.screenplay.targets.Target;
 public class AccommodationOccupancyPage {
     public static final Target BTN_SELECT_CHILDREN_OPTION = Target.the("button select option children")
             .locatedBy("//android.view.ViewGroup[@resource-id='com.booking:id/group_config_children_count']/android.widget.LinearLayout/android.widget.Button[2]");
-    public static final Target BTN_OK = Target.the("button confirm select rooms and guests")
+    public static final Target BTN_OK = Target.the("button OK")
             .located(AppiumBy.id("android:id/button1"));
     public static final Target BTN_CONFIRM_SELECT_ROOMS_AND_GUESTS = Target.the("button confirm select rooms and guests")
             .located(AppiumBy.id("com.booking:id/group_config_apply_button"));
